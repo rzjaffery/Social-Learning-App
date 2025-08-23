59cfb7462accf9aef8d5167b39fdc64410e5ba50
Rayyan Zafar Jaffery <rzjaffery7@gmail.com>
2025-08-23 23:46:31 +0500

    Update Question.java

--- app/src/main/java/com/rzjaffery/sociallearningapplication/model/Question.java
+++ app/src/main/java/com/rzjaffery/sociallearningapplication/model/Question.java
@@ -1,4 +1,17 @@
 package com.rzjaffery.sociallearningapplication.model;
 
+import java.util.List;
+
 public class Question {
+    public String id, text;
+    public List<String> options;
+    public int correct;
+
+    public Question() {}
+    public Question(String id, String text, List<String> options, int correct) {
+        this.id = id;
+        this.text = text;
+        this.options = options;
+        this.correct = correct;
+    }
 }
