45cf3ac4979ee49fe678261b83ae4dbf4808b5c3
Rayyan Zafar Jaffery <rzjaffery7@gmail.com>
2025-08-23 23:51:46 +0500

    Update TaskItem.java

--- app/src/main/java/com/rzjaffery/sociallearningapplication/model/TaskItem.java
+++ app/src/main/java/com/rzjaffery/sociallearningapplication/model/TaskItem.java
@@ -1,4 +1,19 @@
 package com.rzjaffery.sociallearningapplication.model;
 
 public class TaskItem {
+    public String id, title, description, priority;
+    public String status;
+    public long createdAt;
+
+    public TaskItem() {
+    }
+
+    public TaskItem(String id, String title, String description, String priority, String status, long createdAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
 }
