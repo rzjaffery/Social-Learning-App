8f436162fc916574bff9a119800ba4e547edf6c7
Rayyan Zafar Jaffery <rzjaffery7@gmail.com>
2025-08-23 20:35:50 +0500

    update App.java

--- app/src/main/java/com/rzjaffery/sociallearningapplication/App.java
+++ app/src/main/java/com/rzjaffery/sociallearningapplication/App.java
@@ -1,4 +1,15 @@
 package com.rzjaffery.sociallearningapplication;
 
-public class App {
+import android.app.Application;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.firebase.FirebaseApp;
+
+public class App extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+        MobileAds.initialize(this, initializationStatus -> {});
+    }
 }
