c14a9b9bef0bb6f0e41700fa5e78c66c6bb2cc24
Rayyan Zafar Jaffery <rzjaffery7@gmail.com>
2025-08-23 23:47:42 +0500

    Update QuizAttempt.java

--- app/src/main/java/com/rzjaffery/sociallearningapplication/model/QuizAttempt.java
+++ app/src/main/java/com/rzjaffery/sociallearningapplication/model/QuizAttempt.java
@@ -1,4 +1,15 @@
 package com.rzjaffery.sociallearningapplication.model;
 
 public class QuizAttempt {
+    public String id;
+    public int score, total;
+    public long timestamp;
+
+    public QuizAttempt() {}
+    public QuizAttempt(String id, int score, int total, long timestamp) {
+        this.id = id;
+        this.score = score;
+        this.total = total;
+        this.timestamp = timestamp;
+    }
 }
