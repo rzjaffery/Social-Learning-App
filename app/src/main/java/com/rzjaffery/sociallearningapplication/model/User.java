145e2b1c12cfcfec681096047f268049db046ff3
Rayyan Zafar Jaffery <rzjaffery7@gmail.com>
2025-08-23 23:44:23 +0500

    Update User.java

--- app/src/main/java/com/rzjaffery/sociallearningapplication/model/User.java
+++ app/src/main/java/com/rzjaffery/sociallearningapplication/model/User.java
@@ -1,4 +1,15 @@
 package com.rzjaffery.sociallearningapplication.model;
 
 public class User {
+    public String uid, name, email, photo;
+    public long createdAt;
+
+    public User() {}
+    public User(String uid, String name, String email, String photo, long createdAt) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+        this.photo = photo;
+    }
+
 }
