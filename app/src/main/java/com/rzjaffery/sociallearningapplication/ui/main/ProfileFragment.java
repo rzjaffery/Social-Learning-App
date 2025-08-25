eda4353ce92112cd3786ad7120794b6b9be48a77
Rayyan Zafar Jaffery <rzjaffery7@gmail.com>
2025-08-25 10:41:38 +0500

    Profile Module

--- app/src/main/java/com/rzjaffery/sociallearningapplication/ui/main/ProfileFragment.java
+++ app/src/main/java/com/rzjaffery/sociallearningapplication/ui/main/ProfileFragment.java
@@ -1,4 +1,58 @@
 package com.rzjaffery.sociallearningapplication.ui.main;
 
-public class ProfileFragment {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.*;
+import com.rzjaffery.sociallearningapplication.R;
+import com.rzjaffery.sociallearningapplication.data.repository.QuizRepository;
+import com.rzjaffery.sociallearningapplication.data.repository.TaskRepository;
+
+public class ProfileFragment extends Fragment {
+    private TextView tvQuizCnt;
+    private TextView tvTaskCnt;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inf, ViewGroup parent, Bundle b) {
+        View v = inf.inflate(R.layout.fragment_profile, parent, false);
+        TextView tvName = v.findViewById(R.id.tvName);
+        TextView tvEmail = v.findViewById(R.id.tvEmail);
+        tvQuizCnt = v.findViewById(R.id.tvQuizCount);
+        tvTaskCnt = v.findViewById(R.id.tvTaskCount);
+        var user = FirebaseAuth.getInstance().getCurrentUser();
+        assert user != null;
+        tvEmail.setText(user.getEmail());
+        tvName.setText(user.getDisplayName() == null ? "User" : user.getDisplayName());
+        String uid = user.getUid();
+        new QuizRepository().attemptRef(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snap) {
+                tvQuizCnt.setText("Quiz attempts: " + snap.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError e) {
+            }
+        });
+        new TaskRepository().tasksRef(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snap) {
+                tvTaskCnt.setText("Tasks: " + snap.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError e) {
+            }
+        });
+        return v;
+    }
 }
